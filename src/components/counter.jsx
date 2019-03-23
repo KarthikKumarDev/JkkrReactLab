7bcc047fef5a4ddc20a4bc93b9d3f6da0f030b92
karthikkumar1996 <jkkr.1996@gmail.com>
2019-03-23 20:21:50 +0530

    events, binding event handlers and state update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,26 +6,46 @@ class Counter extends Component {
     tags: ["jkkr", "kk", "Kumar"]
   };
 
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
   render() {
     return (
       <React.Fragment>
-        {this.state.tags.length === 0 && "Please create a new tag"}
-        <div className="mt-5 ml-3">{this.renderTags()}</div>
+        <div className="mt-5 ml-3">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button
+            onClick={this.handleIncrement}
+            className="btn btn-sm btn-secondary"
+          >
+            Increment
+          </button>
+          <ul>
+            {this.state.tags.map(tag => (
+              <li key={tag}> {tag}</li>
+            ))}
+          </ul>
+        </div>
       </React.Fragment>
     );
   }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}> {tag}</li>
-        ))}
-      </ul>
-    );
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
+
+  handleIncrement = () => {
+    console.log("Increment", this);
+    this.setState({ count: this.state.count + 1 });
+  };
 }
 
 export default Counter;
