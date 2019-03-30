19d7eb12c242cf79fd8a5f3518d3862a987ab8dd
karthikkumar1996 <jkkr.1996@gmail.com>
2019-03-30 21:13:51 +0530

    Mounting

--- src/App.js
+++ src/App.js
@@ -8,21 +8,35 @@ class App extends Component {
   state = {
     counters: [{ id: 1, value: 4 }, { id: 2, value: 0 }, { id: 3, value: 0 }]
   };
+
+  constructor() {
+    super();
+    // Initialize the State
+    console.log("App - constructor");
+  }
+
+  componentDidMount() {
+    // AJAX Call
+    console.log("App - mounted");
+  }
   render() {
+    console.log("App - rendered");
     return (
-      <React.Fragment className="App">
-        <NavBar
-          totalCounters={this.state.counters.filter(c => c.value > 0).length}
-        />
-        <main className="">
-          <Counters
-            onReset={this.handleReset}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            counters={this.state.counters}
+      <React.Fragment>
+        <div className="App">
+          <NavBar
+            totalCounters={this.state.counters.filter(c => c.value > 0).length}
           />
-          <Description />
-        </main>
+          <main className="">
+            <Counters
+              onReset={this.handleReset}
+              onDelete={this.handleDelete}
+              onIncrement={this.handleIncrement}
+              counters={this.state.counters}
+            />
+            <Description />
+          </main>
+        </div>
       </React.Fragment>
     );
   }
